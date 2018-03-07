c34644752a5ede9519b4b066bab4b5158acd193d
atmashere <atmashere@ya.ru>
2018-03-07 10:48:47 +0300

    First commit

--- custom.h
+++ custom.h
@@ -5,25 +5,41 @@
 #include "model.h"
 
 //---------------------------------------------------------------------------
-// Задача Аренсторфа (начальные условия 1)
+// Модель математического маятника (начальные условия 1)
 
-class TArenstorfModel  : public TModel
+class TMathPendulum : public TModel
 {
     protected:
-        static const long double m;
-		long double D1, D2;
+        const float g;
+        float m, l, phi;
+
     public:
-        TArenstorfModel(  );
-		void getRight( const TVector& X, long double t, TVector& Y );
-};
+        TMathPendulum( );
+        void getRight( const TVector& X, long double t, TVector& Y );
+        ~TMathPendulum( );
+
+}
+
+//class TArenstorfModel  : public TModel
+//{
+//    protected:
+//        static const long double m;
+//		long double D1, D2;
+//    public:
+//        TArenstorfModel(  );
+//		void getRight( const TVector& X, long double t, TVector& Y );
+//};
 
 
 //---------------------------------------------------------------------------
 // Задача Аренсторфа (начальные условия 2)
 
-class TArenstorfModel2 : public TArenstorfModel
-{
-    public:
-        TArenstorfModel2(  );
-};
+
+
+
+//class TArenstorfModel2 : public TArenstorfModel
+//{
+//    public:
+//        TArenstorfModel2(  );
+//};
 //---------------------------------------------------------------------------
